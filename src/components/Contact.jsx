25d244467f331d8e9a8bc674b050b0cc0f329eb1
mlobeto <mercedeslobeto@gmail.com>
2023-05-14 11:35:08 -0300

    textures3 commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,6 +36,7 @@ const Contact = () => {
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+       
         {
           from_name: form.name,
           to_name: "María Lobeto",
